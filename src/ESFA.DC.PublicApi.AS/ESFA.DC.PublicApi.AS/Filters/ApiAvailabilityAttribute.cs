315e4ec8eb337b41294204f4d1ab4838368cc9fc
Hari Dupati <dupatihari@gmail.com>
2020-10-14 14:12:34 +0100

    105642 - ApiAvailability Check for all the end points except status

---
+++ src/ESFA.DC.PublicApi.AS/ESFA.DC.PublicApi.AS/Filters/ApiAvailabilityAttribute.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ESFA.DC.PublicApi.AS.Filters
+{
+    public sealed class ApiAvailabilityAttribute : TypeFilterAttribute
+    {
+        public ApiAvailabilityAttribute() : base(typeof(ApiAvailabilityFilter))
+        {
+        }
+    }
+}
---
+++ src/ESFA.DC.PublicApi.AS/ESFA.DC.PublicApi.AS/Filters/ApiAvailabilityFilter.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading;
+using ESFA.DC.Logging.Interfaces;
+using ESFA.DC.PublicApi.AS.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace ESFA.DC.PublicApi.AS.Filters
+{
+    public  class ApiAvailabilityFilter : IActionFilter
+    {
+        private readonly ILearnerApiAvailabilityService _learnerApiAvailabilityService;
+        private readonly ILogger _logger;
+
+        public ApiAvailabilityFilter(ILearnerApiAvailabilityService learnerApiAvailabilityService, ILogger logger)
+        {
+            _learnerApiAvailabilityService = learnerApiAvailabilityService;
+            _logger = logger;
+        }
+
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            var apiAvailable = _learnerApiAvailabilityService.IsLearnerApiAvailableAsync(CancellationToken.None).Result;
+            if (!apiAvailable)
+            {
+                _logger.LogDebug($"Learner Api is not available.");
+                context.Result = new NoContentResult();
+            }
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            //do nothing
+        }
+    }
+}
